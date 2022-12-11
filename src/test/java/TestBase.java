2ae8eb7c717558a8c3788de49bdf3684280c819f
Mateusz Tadla <mat@scalepoint.com>
2022-12-11 15:09:15 +0100

    add tests cleaup in test base

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -4,8 +4,15 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
 
 public class TestBase {
     public final static String baseUrl = Configuration.getBaseUrl();
@@ -14,8 +21,9 @@ public class TestBase {
     public final static String cards = "/cards";
     public final static String actions = "/actions";
     public RequestSpecification reqSpec;
+
     @BeforeMethod
-    public void testSetup(){
+    public void testSetup() {
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
 
         RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
@@ -24,6 +32,32 @@ public class TestBase {
         reqBuilder.setContentType(ContentType.JSON);
 
         reqSpec = reqBuilder.build();
+    }
+
+    @AfterSuite
+    public void testsCleaup() {
+        Response response =
+                given()
+                        .spec(reqSpec)
+                        .when()
+                        .pathParam("id", "postman349")
+                        .get(baseUrl + "/organizations/{id}/boards")
+                        .then()
+                        .statusCode(200)
+                        .extract().response();
+
+        JsonPath jsonPath = response.jsonPath();
+        List<String> allBoardIds = jsonPath.getList("id");
+
+        for (String id : allBoardIds) {
+            given()
+                    .spec(reqSpec)
+                    .pathParam("id", id)
+                    .when()
+                    .delete(baseUrl + boards + "/{id}")
+                    .then()
+                    .statusCode(200);
+        }
     }
 
 }
