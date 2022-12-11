db08ed9deceefeb203221696712b445cc4328c7f
Mateusz Tadla <mat@scalepoint.com>
2022-12-11 14:43:23 +0100

    add net test class

---
+++ src/test/java/TrelloE2eTests.java
@@ -0,0 +1,58 @@
+import org.testng.annotations.Test;
+
+public class TrelloE2eTests extends TestBase {
+    private static String boardId;
+    private static String todoId;
+    private static String doneId;
+    private static String cardId;
+    private static String actionId;
+
+    @Test(priority = 1)
+    public void shouldCreateBoardWithoutDefaultLists() {
+
+    }
+
+    @Test(priority = 2)
+    public void shouldCreateDoneList() {
+
+    }
+
+    @Test(priority = 3)
+    public void shouldCreateTodoList() {
+
+    }
+
+
+    @Test(priority = 4)
+    public void shouldCreateCardOnTodo() {
+
+    }
+
+
+    @Test(priority = 5)
+    public void shouldAddCommentToCar() {
+
+    }
+
+
+    @Test(priority = 6)
+    public void shouldLikeComment() {
+
+    }
+
+
+    @Test(priority = 7)
+    public void shouldMoveCardToDoneList() {
+
+    }
+
+    @Test(priority = 8)
+    public void shouldDeletedBoard() {
+
+    }
+
+    @Test(priority = 9)
+    public void shouldGetDeletedBoard() {
+
+    }
+}
